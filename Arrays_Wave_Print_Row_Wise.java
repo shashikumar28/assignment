98db4e3b20ec68fb160cc7e23cb0a91e5abcec4f
Shashi Kumar <161289011+shashikumar28@users.noreply.github.com>
2025-06-08 23:27:26 +0530

    Update Arrays_Wave_Print_Row_Wise.java

--- Arrays_Wave_Print_Row_Wise.java
+++ Arrays_Wave_Print_Row_Wise.java
@@ -10,7 +10,7 @@ public class Arrays_Wave_Print_Row_Wise {
         int[][] arr = new int[n][m];
         for(int row = 0; row< arr.length; row++){
             for(int col = 0; col <arr[0].length;col++){
-                arr[row][col] = sc.nextInt();
+                arr[row][col]  = sc.nextInt();
             }
         }
         rowprint(arr);
